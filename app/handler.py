0808e65e52a977def003333f5678c3ce9bb5ef8c
Igor Kovorotniy <i.kovorotniy@rightech.io>
2019-11-20 14:57:56 +0300

    Reupdated main logic, refactoring

--- app/handler.py
+++ app/handler.py
@@ -22,34 +22,33 @@ odoo = odoorpc.ODOO(ODOO_HOST, protocol='jsonrpc', port=ODOO_PORT)
 logging.basicConfig(filename='app.log', filemode='w',
                     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
-# function stays for person job position
-# title stays for person appeal like miss, mister and etc.
-# odoo_contact_fields = [
-#     'name', 'phone', 'mobile', 'email', 'street', 'street2', 'city', 'function', 'website', 'title'
+# possible_odoo_fields = [
+# 'name', 'partner_name', 'contact_name', 'street', 'street2', 'city', 'country', 'function', 
+# 'email_from', 'phone', 'mobile', 'email', 'website', 'title'
 # ]
 
-possible_odoo_fields = [
-    'name', 'partner_name', 'contact_name', 'street', 'street2', 'city', 'country', 'function', 'email_from', 'phone', 'mobile', 'email', 'website', 'title'
-]
-
-
-# try to login to odoo
-try:
-    odoo.login(ODOO_DB, login=ODOO_LOGIN, password=ODOO_PASSWORD)
-except odoorpc.error.RPCError:
-    logging.warning("Can't login successfully to odoo")
+# dict keys are lending values, dict values are odoo fields
+lending_toOdoo_fields = {'project': 'name', 'name': 'contact_name',
+                         'company': 'partner_name', 'email': 'email_from', 'phone': 'phone'}
 
 
 @app.route('/create/new/<string:record>', methods=["POST"])
 def create_new_entity(record):
     """ Create new entity which depends on request record variable """
-    logging.info("Get request to create new %s", record)
+    # try to login to odoo
+    try:
+        odoo.login(ODOO_DB, login=ODOO_LOGIN, password=ODOO_PASSWORD)
+    except odoorpc.error.RPCError:
+        logging.warning("Can't login successfully to odoo")
+        return Response(status=500)
 
+    logging.info("Get request to create new %s", record)
     data = request.form
     token = data.get('token', (None))
     # there will be parameters to create new item in odoo
     odoo_fields = {}
-    odoo_env = {'contact': odoo.env['res.partner'], 'lead': odoo.env['crm.lead']}
+    odoo_env = {'contact': odoo.env['res.partner'],
+                'lead': odoo.env['crm.lead']}
     if not token:
         print("Token doesn't exist")
         logging.warning("Token doesn't exist in request data")
@@ -62,11 +61,11 @@ def create_new_entity(record):
             return Response(status=401)
 
     if decoded_token:
-        items = data.items()
-        for item_key, item_value in items:
-            for key in possible_odoo_fields:
-                if key == item_key:
-                    odoo_fields.update({str(item_key): str(item_value)})
+        payload = data.items()
+        for payload_key, payload_value in payload:
+            for key, value in lending_toOdoo_fields.items():
+                if payload_key == key:
+                    odoo_fields.update({str(value): str(payload_value)})
         try:
             current_env = odoo_env.get(record, (None))
             if not current_env:
@@ -75,6 +74,14 @@ def create_new_entity(record):
             elif current_env == odoo.env['res.partner']:
                 odoo_fields.update({"company_type": "person"})
 
+                # Using the same lending_to_odoo_fields for contacts and leads
+                # some hack below, because of not properly known format
+                try:
+                    odoo_fields["name"] = odoo_fields.pop("contact_name")
+                    odoo_fields["email"] = odoo_fields.pop("email_from")
+                except KeyError:
+                    logging.warning("Key error while redefining fields from leads to contanct")
+                
                 print("total fields if contact", odoo_fields)
                 current_env.create(odoo_fields)
             elif current_env == odoo.env['crm.lead']:
@@ -85,88 +92,3 @@ def create_new_entity(record):
             return Response(status=500)
 
     return Response(status=201)
-
-
-# @app.route('/create/contact', methods=["POST"])
-# def create_odoo_contact():
-#     """ Create contact in odoo, if token is valid, in other case return 401 code """
-#     logging.info("Get request for creating contact")
-#     data = request.form
-
-#     token = data.get('token', (None))
-
-#     if not token:
-#         print("token doesnt exist")
-#         logging.warning("Token doesnt exist in request")
-#         return Response(status=401)
-#     else:
-#         # set default company type for all external contacts
-#         exist_odoo_fields = {"company_type": "person"}
-#         #get key, values from data
-#         items = data.items()
-
-#         try:
-#             decoded_token = jwt.decode(token, JWT_SECRET, ['HS256'])
-#         except jwt.exceptions.InvalidSignatureError as error:
-#             logging.warning('Invalid token error. Error: %s', error)
-#             return Response(status=401)
-
-#         if decoded_token:
-#             for item_key, item_value in items:
-#                 for key in odoo_contact_fields:
-#                     if key == item_key:
-#                         # add key:value to dict
-#                         exist_odoo_fields.update({str(item_key): str(item_value)})
-#             print(exist_odoo_fields)
-#             try:
-#                 contact = odoo.env['res.partner']
-#                 # create contact from dict
-#                 contact.create(exist_odoo_fields)
-#             except odoorpc.error.RPCError as error:
-#                 logging.warning("Odoo rpc error! Error: %s", error)
-#                 return Response(status=500)
-
-#     return Response(status=201)
-
-# @app.route("/create/lead", methods=["POST"])
-# def create_odoo_lead():
-#     """ Create lead in odoo. If token is valid """
-#     logging.info("Get request for creating lead")
-
-#     data = request.form
-
-#     exist_odoo_fields = {}
-
-#     token = data.get('token', (None))
-
-#     if not token:
-#         print("Token doesn't exist")
-#         logging.warning("Token doesn't exist in request")
-#         return Response(status=401)
-#     else:
-#         items = data.items()
-
-#         try:
-#             decoded_token = jwt.decode(token, JWT_SECRET, ['HS256'])
-#         except jwt.exceptions.InvalidSignatureError as error:
-#             logging.warning('Invalid token error. Error: %s', error)
-#             return Response(status=401)
-
-#         if decoded_token:
-#             for item_key, item_value in items:
-#                 for key in odoo_lead_fields:
-#                     if key == item_key:
-#                         # add key:value to dict
-#                         exist_odoo_fields.update({str(item_key): str(item_value)})
-#                         print(key, item_value)
-
-#             print(exist_odoo_fields)
-#             try:
-#                 lead = odoo.env['crm.lead']
-#                 # create lead from dict
-#                 lead.create(exist_odoo_fields)
-#             except odoorpc.error.RPCError as error:
-#                 logging.warning("Odoo rpc error! Error: %s", error)
-#                 return Response(status=500)
-
-#     return Response(status=201)
